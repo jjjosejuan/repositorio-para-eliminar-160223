708c1aa878dd44848f05e321893a1028ab4e3ec7
josejuan <josebartoli05@gmail.com>
2023-02-19 00:51:57 -0600

    envio committ sabado 12:51

--- archivo2repositorioparaeliminar17170223.js
+++ archivo2repositorioparaeliminar17170223.js
@@ -1,4 +1,5 @@
 //realizando pruebas viernes 17 febrero 2023.
 //realizando commit y cambios con consola.
 //Realizando prueba de envio desde visual sabado 190223 12:47
+//Realizando prueba de envio de commit desde visual 190223
 
